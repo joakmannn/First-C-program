838364ef3c83ca38888a91c902bc8d70081cfd9a
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-01-08 16:06:18 +0100

    Varaibles

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,16 +2,27 @@
 
 int main(){
 
-    /* escape sequence = character comination consisting of a backslash \
-                         Followed by a letter or combination of digits.
-                         Ther specify actions within a line or string of text.
-                         \n line
-                         \t tab
-    
-    */
+    // Variable = Allocated space in memory to store a value.
+    //            We refer to a variable's name to access the stored value.
+    //            That variable now behaves as if it was the value it contains.
+    //            BUT we need to declare what type of data we are storing. 
 
-    printf("I like pizza\n");
-    printf("I like pizza");
+    
+    int x; //declaration
+    x = 123; //initialisation
+    int y = 321; //declaration + initialisation
+
+    int age = 21; //integer %d
+    float gpa = 2.05; //floating point number %f
+    char grade = 'C'; //single character %c
+    char name[] = "Bro"; //array of characters %s
+
+    printf("Hello %s\n",name);
+    printf("You are %d years old\n",age);
+    printf("Your average grade is : %c\n",grade);
+    printf("You gpa is : %f\n",gpa);
+
+
 
     return 0;
 }
