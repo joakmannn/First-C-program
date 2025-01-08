912443831dcf8e7aaad90ed54503f042a64d4642
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-01-08 15:43:38 +0100

    Escape sequences

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,14 @@
 
 int main(){
 
+    /* escape sequence = character comination consisting of a backslash \
+                         Followed by a letter or combination of digits.
+                         Ther specify actions within a line or string of text.
+                         \n line
+                         \t tab
+    
+    */
+
     printf("I like pizza\n");
     printf("I like pizza");
 
